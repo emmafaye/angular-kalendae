3e217d1f962e82335e08f2d45ddfa46a03b518f1
Matthew Hardnack <matthewhardnack@gmail.com>
2013-09-16 21:48:32 -0700

    Added bower.json and src.

---
+++ angular.kalendae.js
@@ -0,0 +1,92 @@
+// Requires kalendae.js and moment.js
+
+var module = angular.module('Kalendae', []);
+
+module.factory('Moment', ['$window', function($window) {
+    return $window.moment;
+}]);
+
+module.directive('kalendae', ['$parse', 'KalendaeAPI', function($parse, KalendaeAPI) {
+    return {
+        restrict: 'EA',
+        link: function(scope, element, attrs) {
+            var model      = $parse(attrs.ngModel);
+
+            var type       = attrs.kalendae === 'dropdown' ? Kalendae.Input : Kalendae;
+            var datePicker = new type(element[0], {
+                mode       : attrs.mode        || 'single',
+                months     : attrs.months      || 1,
+                useYearNav : attrs.useYearNav  || true,
+                direction  : attrs.direction   || 'any',
+                closeButton: attrs.closeButton || false
+            });
+            KalendaeAPI.setDatePicker(datePicker);
+
+            // Requires animate.css in order to animate.
+            angular.element(datePicker.container).addClass('animated fadeInDown');
+
+            datePicker.subscribe('date-clicked', function(date) {
+                var selectedDate = KalendaeAPI.setDate(date);
+
+                model.assign(scope, selectedDate);
+
+                // When using the dropdown datepicker, hide after selecting a date.
+                attrs.kalendae === 'dropdown' && element[0].blur();
+            });
+        }
+    };
+}]);
+
+module.factory('KalendaeAPI', ['Moment', function(Moment) {
+    return {
+        datePicker: null,
+        values:null,
+        dates:[],
+        setDatePicker: function(datePicker) {
+            return this.datePicker = datePicker
+        },
+        setSelected: function(date) {
+            this.datePicker.setSelected(date);
+        },
+        setBlackOutDates: function() {
+
+        },
+        setValues: function(values) {
+            this.values = values;
+        },
+        setDate: function(date) {
+            return Moment(date).format('MM/DD/YYYY');
+        },
+        setDates: function(date) {
+            this.dates = [];
+            for(var key in dates) {
+                var date = Moment(dates[key]).format('MM/DD/YYYY');
+
+                this.dates.push(date);
+            }
+
+            return this.dates;
+        },
+        getDates: function() {
+            return this.dates;
+        },
+        getValueByDate: function(date) {
+            var valueByDate;
+            var date = Moment(date);
+
+            for(var key in this.values) {
+                var min_date = this.values[key].min;
+                var max_date = this.values[key].max;
+                var value    = this.values[key].value;
+
+
+                if(date.isBefore(max_date) && date.isAfter(min_date)) {
+                    valueByDate = value;
+                    break;
+                }
+            }
+
+            return valueByDate;
+        }
+    };
+}]);
